574c5b668e05d3da22b05a4ecdce4f2d72f8fac2
unknown <dharmi.mys@gmail.com>
2022-06-29 19:54:23 +0530

    while & for loop updated

---
+++ program1/loop/for1.sh
@@ -0,0 +1,14 @@
+for (( a=1; a<6; a++))
+do
+echo $a
+done
+
+for (( a=1; a<6; a+=2 ))
+do
+echo $a
+done
+
+for (( a=5; a>0; a--))
+do
+echo $a
+done
---
+++ program1/loop/while1.sh
@@ -0,0 +1,10 @@
+a=1
+
+
+while [ $a -lt 6 ]
+do
+echo $a
+
+a=$((a+1))
+
+done
